008c6941c0bb8f65ad9aa60c38dab235a1632d15
charles-bezerra <charlesbezerra5@gmail.com>
2020-05-23 19:24:19 -0300

    Quick Sort Added

--- quick_sort/main.go
+++ quick_sort/main.go
@@ -1,19 +1,64 @@
 package main
 
-
 import (
 	"fmt"
 )
 
-func quick(left []int, right []int) []int {
-	return left
-}
+func quick_sort(list []int) []int {
+	length := len(list)
 
-func sort(list []int) []int {
+	if length < 2 {
+		return list
+	}
+
+	pivo_index := length - 1
+	pivo := list[pivo_index]
+	i := length - 2
+
+	for i >= 0 && pivo < list[i] {
+		list[i], list[i+1] = pivo, list[i]
+		pivo_index = i
+		i--
+	}
+
+	if length > 2 {
+		left := quick_sort(list[:pivo_index])
+		right := quick_sort(list[pivo_index+1:])
+
+		j := 0
+
+		for i = 0; i < length; i++ {
+			if i < pivo_index {
+				list[i] = left[i]
+
+			} else if i > pivo_index {
+				list[i] = right[j]
+				j++
+
+			}
+		}
+	}
+
 	return list
 }
 
-func main()  {
-	list := []int{1,30,49,40,42,30,40}
-	fmt.Println(sort(list))
+func main() {
+	var n int = 0
+
+	fmt.Print("Enter With Numbers Of Entries: ")
+	fmt.Scanln(&n)
+
+	list := make([]int, n)
+
+	fmt.Print("List: ")
+	for i := 0; i < n; i++ {
+		fmt.Scan(&list[i])
+	}
+
+	fmt.Print("Sorted List:")
+	for _, e := range quick_sort(list) {
+		fmt.Print(" ", e)
+	}
+
+	fmt.Println("")
 }
--- test.go
+++ test.go
@@ -3,13 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	const v int = 120
-	fmt.Printf("%T - %v\n", v, v)
+	// const v int = 120
+	// fmt.Printf("%T - %v\n", v, v)
 
-	j := []int{1, 2, 3, 4, 5}
+	// j := []int{1, 2, 3, 4, 5}
 
-	for len(j) > 0 {
-		fmt.Println(j)
-		j = j[1:]
-	}
+	// for len(j) > 0 {
+	// 	fmt.Println(j)
+	// 	j = j[1:]
+	// }
+
+	l := []int{1, 2, 3, 4}
+	fmt.Println(l[1:2])
 }
